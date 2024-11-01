4a5aa621ed29fbb3921e4f54238ad9ab26ddb9ed
Ky Dang <167521177+KeithDang1610@users.noreply.github.com>
2024-11-01 19:39:05 +0700

    Add files via upload

---
+++ Week4/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("cruise_ship_model.pkl")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features= [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0],4)
+    return render_template("index.html",prediction_text="crew should be{}".format(output))
+    
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
